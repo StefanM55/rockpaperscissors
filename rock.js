c81518a4039bfcb7aa7250e343d1a7da552f763c
StefanM <stefanmiloiu@live.com>
2022-08-17 21:20:14 +0300

    bug rezolvat - declarare vars + acoperire cazuri

--- rock.js
+++ rock.js
@@ -1,6 +1,7 @@
 var score;
-var user;
-var comp;
+var result;
+var user=0;
+var comp=0;
 function getComputerChoice(){
     var gameArray = [
         "rock",
@@ -13,46 +14,50 @@ function getComputerChoice(){
 }
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection=="rock" && computerSelection=="paper"){
-        var result = "you lose! Paper beats rock";
-        var score = "2";
+    if (playerSelection==="rock" && computerSelection==="paper"){
+        result = "you lose! Paper beats rock";
+        score = 2;
     }
-    else if (playerSelection=="rock" && computerSelection=="scissors"){
-        var result = "you win! rock beats scissors";
-        var score = "1";
+    else if (playerSelection==="rock" && computerSelection==="scissors"){
+         result = "you win! rock beats scissors";
+        score = 1;
     }
-    else if (playerSelection=="scissors" && computerSelection=="paper"){
-        var result = "you win! rock beats paper";
-        var score = "1";
+    else if (playerSelection==="scissors" && computerSelection==="paper"){
+         result = "you win! rock beats paper";
+         score = 1;
     }
-    else if (playerSelection=="scissors" && computerSelection=="rock"){
-        var result = "you lose! rock beats scissors";
-        var score = "2";
+    else if (playerSelection==="scissors" && computerSelection==="rock"){
+         result = "you lose! rock beats scissors";
+         score = 2;
     }
-    else if (playerSelection=="paper" && computerSelection=="scissors"){
-        var result = "you lose! scissors beat paper";
-        var score = "2";
+    else if (playerSelection==="paper" && computerSelection==="scissors"){
+         result = "you lose! scissors beat paper";
+         score = 2;
     }
-    else if (playerSelection=="paper" && computerSelection=="rock"){
-        var result = "you win! paper beats rock";
-        var score = "1";
+    else if (playerSelection==="paper" && computerSelection==="rock"){
+         result = "you win! paper beats rock";
+         score = 1;
     }
     else {
-        var result = "TIE";
-        var score = "0";
+        result = "TIE";
+         score = 0;
     }
     return result, score;
   }
 
 function game(){
-    const playerSelection = "rock";
+    let playerSelection = (window.prompt("Choose rock-paper-scissors","rock")).toLowerCase();
     for(let i = 0; i < 5; i++){
-        
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection)
-        if (score == "1"){
+      
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        console.log(playerSelection);
+        playRound(playerSelection, computerSelection);
+        console.log(result);
+        console.log(score);
+        if (score === 1){
             user++
-        } else if (score == "2") {
+        } else if (score === 2) {
             comp++
         }
         else{
@@ -62,12 +67,16 @@ function game(){
     }
     console.log(comp)
     console.log(user)
-    if (comp>>user){
-        var finnish = "YOU LOSE"+ comp + "-" + user; 
-    } else {
-        var finnish = "YOU WIN"+ comp + "-" + user; 
+    if (comp>user){
+        var finnish = "YOU LOSE";
+    } else if (user<comp){
+        var finnish = "YOU WIN";
     }
-    return finnish;  
+    else{
+        var finnish = "it's a TIE";
+    }
+    return finnish; 
+     
 }
 console.log(game());
 
